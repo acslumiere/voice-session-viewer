73fecbab1e22eeb867d657d03c0575acb93c75db
acslumiere <acslumiere@gmail.com>
2025-05-01 20:01:16 -0500

    Modified page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,9 @@
-a welcome message
+// app/page.tsx
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Voice Session Viewer</h1>
+      <p>Your Firebase test page is <a href="/test-firebase">here</a>.</p>
+    </div>
+  );
+}
